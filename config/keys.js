e3f83040690449fc72e0c8df5e1c291229433b6c
Guan <33585352+GuanYangCLU@users.noreply.github.com>
2019-05-07 04:15:32 -0700

    delete keys

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://guan:yangguan@db0-qtvf2.mongodb.net/test?retryWrites=true",
+    "",
   secretOrKey: "secret",
-  githubClientId: "2a73ee13d0751186914b",
-  githubSecret: "0752ef2e5f728791b3a37cf53e7ff69916f2232e"
+  githubClientId: "",
+  githubSecret: ""
 };
